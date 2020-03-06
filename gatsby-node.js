99f0a6bd24a2f4823b41bfb4edb40cd4213fa7fc
Yoann Fleury <yoann.fleury@yahoo.com>
2020-03-06 13:45:04 +0100

    :rocket: add netlify redirect plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createRedirect } = actions
+  createRedirect({
+    fromPath: "/2019",
+    toPath: "https://archive.codeursenseine.com/2019",
+    isPermanent: true,
+  })
+}
