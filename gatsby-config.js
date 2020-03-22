24b225468b1dd3c0cb5e968d0cf0450656146d02
Ivan Dalmet <ivan@dalmet.fr>
2020-03-22 20:06:06 +0100

    feat: Add Chakra-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,16 +18,23 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `codeurs-en-seine`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#034ea2`,
+        theme_color: `#034ea2`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-chakra-ui`,
+      options: {
+        isResettingCSS: true,
+        isUsingColorMode: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/gatsby-plugin-chakra-ui/theme.js
@@ -0,0 +1,31 @@
+import { theme } from "@chakra-ui/core"
+
+export default {
+  ...theme,
+  shadows: {
+    ...theme.shadows,
+    paper: `
+      rgba(32, 47, 71, 0.06) 0px 0.8125rem 0.4375rem -0.4375rem,
+      rgba(32, 47, 71, 0.08) 0.625rem 0.25rem 0.4375rem -0.5rem,
+      rgba(32, 47, 71, 0.08) -0.625rem 0.25rem 0.4375rem -0.5rem,
+      rgba(32, 47, 71, 0.06) 0px 0.1875rem 0.375rem 0px,
+      rgba(32, 47, 71, 0.04) 0px -0.25rem 0.5rem -0.125rem,
+      rgba(32, 47, 71, 0.03) 0px 0px 0px 0.0625rem;
+    `
+  },
+  colors: {
+    ...theme.colors,
+    brand: {
+      50: '#def3ff',
+      100: '#b0d7ff',
+      200: '#80bcfe',
+      300: '#50a1fc',
+      400: '#2386fa',
+      500: '#0e6ce0',
+      600: '#034ea2',
+      700: '#003c7e',
+      800: '#00244e',
+      900: '#000d1f',
+    },
+  }
+};
