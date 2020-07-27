d319691362eb40286533de6e51e8c3dc45129486
Yoann Fleury <yoann.fleury@yahoo.com>
2020-07-27 22:34:21 +0200

    feat: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
         background_color: `#034ea2`,
         theme_color: `#034ea2`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ces.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
