20b0a9c3d93046d4673668af8b3e3960bc8469e0
Ivan Dalmet <ivan@dalmet.fr>
2020-07-27 22:45:00 +0200

    feat: Add title in PageHeader

--- src/components/Nav/NavTopbar.js
+++ src/components/Nav/NavTopbar.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Flex, IconButton, useTheme } from "@chakra-ui/core"
+import { Box, Text, Heading, Flex, IconButton, useTheme } from "@chakra-ui/core"
 import { FiMenu } from "react-icons/fi"
 import { Logo } from "../Logo"
 import { Link } from "gatsby"
@@ -12,6 +12,7 @@ export const NavTopbar = ({ onNavOpen = () => {}, ...props }) => {
       as="nav"
       d={{ md: "none" }}
       background={theme.gradients.brand}
+      justifyContent="space-between"
       color="white"
       position="fixed"
       top="0"
@@ -23,14 +24,21 @@ export const NavTopbar = ({ onNavOpen = () => {}, ...props }) => {
     >
       {/* TODO: if possible, set the link to current year to avoid multiple redirect */}
       <Link to="/">
-        <Logo w="4.5" h="2.5rem" />
+        <Logo w="4.5" h="2.5rem" pl="2" />
       </Link>
+      <Box textAlign="center">
+        <Text fontFamily="heading" fontSize="0.6rem">
+          {theme.data.pretitle}
+        </Text>
+        <Heading as="h4" fontSize="0.7rem">
+          {theme.data.title}
+        </Heading>
+      </Box>
       <IconButton
         variant="unstyled"
         aria-label="Menu"
         d="inline-flex"
         icon={FiMenu}
-        ml="auto"
         onClick={() => onNavOpen()}
       />
     </Flex>
