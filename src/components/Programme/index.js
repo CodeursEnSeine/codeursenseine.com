35bff0a7c3425f233cadfb11f4b236b9e691a2aa
Asceline B <asceline.boullen@bearstudio.fr>
2020-09-29 20:51:31 +0200

    Implement drawer for conferennce details

--- src/components/Programme/index.js
+++ src/components/Programme/index.js
@@ -1,18 +1,12 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import {
-  Heading,
   Stack,
-  Text,
-  Button,
-  Flex,
-  Badge,
-  Icon,
   SimpleGrid,
 } from "@chakra-ui/core"
 import dayjs from "dayjs"
 import "dayjs/locale/fr"
-import { Card } from "components/Card"
+import { ConferenceCard } from "components/Programme/_partials/ConferenceCard"
 
 export const Programme = () => {
   const data = useStaticQuery(graphql`
@@ -30,7 +24,8 @@ export const Programme = () => {
               startHour,
               endHour,
               speaker,
-              isKeynote
+              isKeynote,
+              description,
             }
           }
         }
@@ -40,44 +35,14 @@ export const Programme = () => {
 
   dayjs.locale('fr');
 
-  const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-
   return (
     <Stack mt={5}>
       <SimpleGrid columns={[1, 1, 1, 2, 2]}>
         {data.allFile.nodes.map((conference, index) => (
-          <Flex key={`conference-${index}`} mt={3}>
-            <Stack flex="0.3" p={5}>
-              <Flex align="center">
-                <Text>{capitalizeFirstLetter(dayjs(conference.childMdx.frontmatter.eventDate).format('dddd D MMM'))}</Text>
-                <Icon name="chevron-right" ml={2} />
-              </Flex>
-              <Text>
-                {`${conference.childMdx.frontmatter.startHour} - ${conference.childMdx.frontmatter.endHour}`}
-              </Text>
-              {conference.childMdx.frontmatter.isKeynote && (
-                <Badge
-                  variantColor="brand"
-                  width="fit-content"
-                >
-                  Keynote
-                </Badge>
-              )}
-            </Stack>
-            <Card flex="0.7">
-              <Heading fontSize="md">{conference.childMdx.frontmatter.title}</Heading>
-              <Text mt={2}>{conference.childMdx.frontmatter.speaker}</Text>
-              <Button
-                as="a"
-                variantColor="brand"
-                variant="link"
-                width="fit-content"
-                mt={2}
-              >
-                Voir le détails et s'inscrire
-              </Button>
-            </Card>
-          </Flex>
+          <ConferenceCard
+            key={`conference-${index}`}
+            conference={conference}
+          />
         ))}
       </SimpleGrid>
     </Stack>
