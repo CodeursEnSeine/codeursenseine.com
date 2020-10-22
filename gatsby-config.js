dfb4d9c8083645265658ad14487494fc0b335d7f
Tatiana <43054051+tacoelho@users.noreply.github.com>
2020-10-22 16:34:14 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,14 +134,4 @@ module.exports = {
       },
     },
   ],
-}
-// this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-
-   // Must be at the end of the plugins
-    `gatsby-plugin-client-side-redirect`,
-  {
-
-    };
-
+};
