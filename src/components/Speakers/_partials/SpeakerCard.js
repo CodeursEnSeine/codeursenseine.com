aa112c4e217875d86b77e448a1f7bdb0f88d855b
Asceline B <asceline.boullen@bearstudio.fr>
2020-10-26 18:34:29 +0100

    WIP page speakers layout

--- src/components/Speakers/_partials/SpeakerCard.js
+++ src/components/Speakers/_partials/SpeakerCard.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "dayjs/locale/fr";
+import {
+  Heading,
+  Text,
+  Button,
+  Flex,
+  Box,
+  Image,
+  AspectRatioBox,
+  IconButton,
+} from "@chakra-ui/core";
+import { FaTwitter, FaGithub} from "react-icons/fa";
 import { Card } from "components/Card";
 
 export const SpeakerCard = ({ speaker }) => {
   const {
     name,
     image: {
-      publicUrl
+      publicURL
     },
     company,
     bio,
@@ -15,12 +26,70 @@ export const SpeakerCard = ({ speaker }) => {
     githubLink,
   } = speaker?.childMdx?.frontmatter;
 
+  console.log(publicURL);
   return (
     <Card
       borderLeftWidth={2}
       borderLeftColor="brand.600"
     >
-      {name}
+      <Flex>
+        <Box mr={4}>
+          <AspectRatioBox ratio={1} w="6em" maxW="100%">
+            <Image src={publicURL} borderRadius={4}/>
+          </AspectRatioBox>
+        </Box>
+        <Box>
+          <Heading fontSize="lg">
+            {name}
+          </Heading>
+          <Heading fontSize="md">
+            {company}
+          </Heading>
+          {twitterLink && (
+            <IconButton
+              mt={2}
+              key="Twitter"
+              as="a"
+              target="_blank"
+              href={twitterLink}
+              aria-label={`${name} Twitter`}
+              icon={FaTwitter}
+              variant="ghost"
+              variantColor="brand"
+              size="md"
+              d="inline-flex"
+            />
+          )}
+          {githubLink && (
+            <IconButton
+              mt={2}
+              key="Github"
+              as="a"
+              target="_blank"
+              href={githubLink}
+              aria-label={`${name} Github`}
+              icon={FaGithub}
+              variant="ghost"
+              variantColor="brand"
+              size="md"
+              d="inline-flex"
+            />
+          )}
+        </Box>
+      </Flex>
+      <Box>
+        <Text mt={4}>{bio}</Text>
+        {References && (
+          <Box mt={5}>
+            <Heading fontSize="sm">
+              Références
+            </Heading>
+            <Text mt={1}>
+              {References}
+            </Text>
+          </Box>
+        )}
+      </Box>
     </Card>
   );
 };
