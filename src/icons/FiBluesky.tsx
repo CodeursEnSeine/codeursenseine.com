f7a7e77bd93730371883c021eabcfed58bba3670
ypetit <public@ypetit.net>
2025-10-22 19:15:43 +0200

    fix: update program, fix speakers bugs, add other social netwoks links

---
+++ src/icons/FiBluesky.tsx
@@ -0,0 +1,15 @@
+import { IconBase, type IconBaseProps } from "react-icons";
+import { siBluesky } from "simple-icons";const FiBluesky = (props: IconBaseProps) => (
+  <IconBase viewBox="-3 0 27 26" {...props}>
+    <path
+      d={siBluesky.path}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </IconBase>
+);
+
+export default FiBluesky;
